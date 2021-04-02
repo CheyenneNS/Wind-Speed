498ec65cdb3b7a078fcac58cd91b1cac34402e9a
Cheyenne Nelson <chychy@As-MacBook-Air.local>
2021-04-01 17:53:13 -0700

    WindSpeed main changed to include python scheduler.

--- WindSpeedSJ.py
+++ WindSpeedSJ.py
@@ -12,26 +12,22 @@ unit-option:
 
 from docopt import docopt
 import requests
+import sys
+import schedule
 import time
+import datetime
 
+def main():
 
-def check_windspeed(unit="imperial"):
-   last_speed = float('-inf')
-   units = {"imperial": "mi/h", "metric": "m/s"}
-   while True:
-      response = requests.get(
-         "https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=3207703ee5d0d14e6b6a53d10071018f&units=" + unit)
-      wind_data = response.json()['wind']
-      wind_speed = float(wind_data['speed'])
-      degrees = int(wind_data['deg'])
-      if wind_speed != last_speed:
-         last_speed = wind_speed
-         direction = wind_direction(degrees)
-         result = str(wind_data['speed']) + " " + units[unit] + " " + direction
-         print(result)
-      else:
-         print("skiped")
-      time.sleep(60)
+   args = docopt(__doc__)
+   print(args)
+   global velocity
+   velocity = float('-inf')
+   scheduler(args)
+
+#class WindData(object):
+   #def __init__(self, velocity):
+      #self.velocity = velocity
 
 
 def wind_direction(degreesFromNorth):
@@ -48,7 +44,7 @@ def wind_direction(degreesFromNorth):
       range(191, 214): 'SSW',
       range(214, 236): 'SW',
       range(236, 259): 'WSW',
-      range(259, 281): 'w',
+      range(259, 281): 'W',
       range(281, 304): 'WNW',
       range(304, 326): 'NW',
       range(326, 349): 'NNW'}
@@ -57,14 +53,41 @@ def wind_direction(degreesFromNorth):
       if degreesFromNorth in k:
          return v
 
-def main():
 
-   args = docopt(__doc__)
-   print(args)
+def exit():
+   print('{} Now the system will exit '.format(datetime.datetime.now()))  # this works
+   sys.exit()
+
+def check_windspeed(unit="imperial"):
+
+   response = requests.get(
+         "https://api.openweathermap.org/data/2.5/weather?q=san+jose&appid=3207703ee5d0d14e6b6a53d10071018f&units=" + unit)
+   wind_data = response.json()['wind']
+   wind_speed = float(wind_data['speed'])
+   global velocity
+
+   if velocity != wind_speed:
+      velocity = wind_speed
+      degrees = int(wind_data['deg'])
+      units = {"imperial": "mi/h", "metric": "m/s"}
+      direction = wind_direction(degrees)
+      result = str(wind_data['speed']) + " " + units[unit] + " " + direction
+      print(result)
+
+
+def scheduler(args):
+
    if args['--metric']:
       check_windspeed('metric')
+      schedule.every(1).minutes.do(check_windspeed, 'metric')
    else:
       check_windspeed()
+      schedule.every(1).minutes.do(check_windspeed)
+   schedule.every(3).minutes.do(exit)
+
+   while True:
+      schedule.run_pending()
+      time.sleep(1)
 
 if __name__ == '__main__':
    main()
